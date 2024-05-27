c4ce948773d616b42dcc11d1cd1f5e2b47c598b7
nikitabiichuk2009 <niktestpython@gmail.com>
2024-05-27 16:07:45 +0300

    change

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,32 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define protected and public routes
+const publicRoutes = [
+  "/",
+  "/api/webhook", // Ensure webhook route is public
+  "/question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+];
+
+// Matchers for protected routes
 const isProtectedRoute = createRouteMatcher(["/ask-question"]);
+const isPublicRoute = createRouteMatcher(publicRoutes);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req) && !isPublicRoute(req)) {
+    auth().protect();
+  }
 });
 
+// Configuring matcher to ignore static files, _next, and specific API routes
 export const config = {
-  matcher: ["/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next|api/webhook|api/chatgpt).*)", // Exclude static files, _next, api/webhook, and api/chatgpt
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
