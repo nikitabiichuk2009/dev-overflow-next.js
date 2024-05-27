684ea756d4c0c840d627675d127c8f367a2d7066
nikitabiichuk2009 <niktestpython@gmail.com>
2024-05-27 14:44:52 +0300

    add api webhook to ingored routes

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,9 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next|api/webhook).*)", // Ignore api/webhook and static files/_next
+    "/",
+    "/(api|trpc)(.*)",
+  ],
 };
