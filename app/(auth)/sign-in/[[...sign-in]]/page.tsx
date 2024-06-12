a8d2e34d039e593afba99af43a4c8e8d88b179f1
nikitabiichuk2009 <niktestpython@gmail.com>
2024-06-12 09:58:20 +0300

    changes

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -14,7 +14,7 @@ const SignInPage = () => {
         className='mb-2 me-2 rounded-lg bg-blue-700 px-5 py-2.5 text-lg
         font-semibold text-white hover:bg-blue-800 dark:bg-blue-600
          dark:hover:bg-blue-700'>Back</Button>
-      <SignIn forceRedirectUrl={"/"} />
+      <SignIn forceRedirectUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL} />
     </main>
   )
 }
